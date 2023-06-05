ad6a67fec71d9d5ebe5e8e71a53c9ae89bf6e106
Himoshra D <114651851+Orcus77@users.noreply.github.com>
2023-06-05 12:58:59 +0530

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (1,n+1):
+        print(i,end="")
+#The end=” “ is used to print in the same line with space after each element. It prints a space after each element in the same line.
+#The end=”” is used to print on same line without space. Keeping the doube quotes empty merge all the elements together in the same line.
+#The end=”,” is used to print in the same line with a comma after each element. We can use some other sign such as ‘.’ or ‘;’ inside the end parameter.
