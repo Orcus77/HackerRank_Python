8693616fcc7b5674cd917ac55902c4bc9a014efb
Himoshra D <114651851+Orcus77@users.noreply.github.com>
2023-06-05 11:31:54 +0530

    Update If-Else.py

--- If-Else.py
+++ If-Else.py
@@ -19,3 +19,24 @@ elif n%2==0 and n>6 and n<20:
     print("Weird")
 elif n%2==0 and n> 20:
     print ("Not Weird")
+##The above code fails test case 7
+##The code below passes all the test cases
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    oddcheck = (n % 2) != 0
+    if oddcheck or (n > 100) or (n < 1) or (n >= 6 and n <= 20):
+        print("Weird")
+    else:
+        print("Not Weird")
